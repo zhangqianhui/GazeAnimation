d5473eff4edbd10bb13b3cb101754163c6e63ae1
zhangqianhui <zhang163220@gmail.com>
2020-08-08 00:18:52 +0200

    sh

--- scripts/test_log8_7.sh
+++ scripts/test_log8_7.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python test.py --exper_name='log3_28_1_17' --gpu_id='0' --crop_h=30 --crop_w=50 --test_sample_dir='test_sample_dir3' --checkpoints='checkpoints2'
+python test.py --exper_name='log3_28_1_17' --gpu_id='0' --crop_h=30 --crop_w=50 --test_sample_dir='test_sample_dir' --checkpoints='checkpoints'
