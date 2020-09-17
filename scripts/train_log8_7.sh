acb6a139680df8cc3a4ad51433402bd29286bb91
zhangqianhui <zhang163220@gmail.com>
2020-09-18 00:46:03 +0200

    test

--- scripts/train_log8_7.sh
+++ scripts/train_log8_7.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py --use_sp --gpu_id='1' --exper_name='log3_28_1_17' --crop_w=50 --crop_h=30
+python train.py --use_sp --gpu_id='1' --exper_name='log3_28_1_17' --crop_w=60 --crop_h=40
